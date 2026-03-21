08f2c45fdba2b61761593fa682b8d28f8637d705
FelipeFuhr <felipefuhr7@gmail.com>
2026-03-21 13:08:21 -0300

    Revert "feat: setup workflow"
    This reverts commit 57591ab14b77a817678f7015590f0fd2a309c2da.

--- examples/hello/stack/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "greeting_path" {
-  description = "Absolute path of the generated greeting file."
-  value       = local_file.greeting.filename
-}
-
-output "greeting_content" {
-  description = "Content written to the greeting file."
-  value       = local_file.greeting.content
-}
--- examples/hello/test/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/ffreis/hello-tf-test
-
-go 1.21
